0abef040ba146955c256204de926394cf5742cdd
borismarin <borismarin@gmail.com>
2017-05-05 12:24:39 -0300

    cleaning up

--- tx_nmodl/tests/test_expr2lems.py
+++ tx_nmodl/tests/test_expr2lems.py
@@ -1,31 +1,5 @@
 from tx_nmodl.expr2lems import Lems
-import xmltodict
-
-def normalise_dict(d):
-    """
-    Recursively convert dict-like object (eg OrderedDict) into plain dict.
-    Sorts list values.
-    """
-    out = {}
-    for k, v in dict(d).items():
-        if hasattr(v, 'items'):
-            out[k] = normalise_dict(v)
-        elif isinstance(v, list):
-            out[k] = []
-            for item in sorted(v, key=lambda x: sorted(x.keys())):
-                if hasattr(item, 'items'):
-                    out[k].append(normalise_dict(item))
-                else:
-                    out[k].append(item)
-        else:
-            out[k] = v
-    return out
-
-
-def xml_compare(a, b):
-    a = normalise_dict(xmltodict.parse(a))
-    b = normalise_dict(xmltodict.parse(b))
-    return a == b
+from xmlcomp import xml_compare
 
 
 def test_dx_func():

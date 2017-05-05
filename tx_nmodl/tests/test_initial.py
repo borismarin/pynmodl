0f6f0bc973adc399b4d26b7f4e86a9a74b1ee11f
borismarin <borismarin@gmail.com>
2017-05-04 21:47:47 -0300

    added initial block

---
+++ tx_nmodl/tests/test_initial.py
@@ -0,0 +1,24 @@
+import os
+from hack import metamodel_with_any_var
+from textx.model import children_of_type
+
+mm = metamodel_with_any_var(
+    os.path.join(os.path.dirname(__file__), '../initial.tx'))
+
+def test_initial():
+    nrn ='''
+    INITIAL {
+        tadj = q10^((celsius - temp)/(10 (degC))) 
+
+        trates(v+vshift)
+        m = minf
+        h = hinf
+    }
+    '''
+    init = mm.model_from_str(nrn)
+    tadk, proc, m0, h0 = init.statements
+    fcall = children_of_type('FuncCall', proc)[0]
+    assert(fcall.func.user.name == 'trates')
+    assert(children_of_type('VarRef', h0.expression).var.name == 'hinf')
+
+

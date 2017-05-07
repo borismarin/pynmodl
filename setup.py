8ddbad7d1a023cd63437490e50f146a5e73cb08d
borismarin <borismarin@gmail.com>
2017-05-07 03:01:00 -0300

    cleaner setup.py

--- setup.py
+++ setup.py
@@ -1,67 +1,16 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-import os
-import sys
-import codecs
 from setuptools import setup
-import tx_nmodl
 
-AUTHOR = 'Boris Marin'
-AUTHOR_EMAIL = 'borismarin@gmail.com'
-GITHUB_ACCOUNT = 'borismarin'
-
-NAME = 'tx_nmodl'
-DESC = 'nmodl textX language'
-VERSION = tx_nmodl.__version__
-LICENSE = 'MIT'
-URL = 'https://github.com/%s/%s' % (GITHUB_ACCOUNT, NAME)
-DOWNLOAD_URL = 'https://github.com/%s/%s/archive/v%s.tar.gz' % \
-    (GITHUB_ACCOUNT, NAME, VERSION)
-README = codecs.open(os.path.join(os.path.dirname(__file__), 'README.md'),
-                     'r', encoding='utf-8').read()
-
-if sys.argv[-1].startswith('publish'):
-    if os.system("pip list | grep wheel"):
-        print("wheel not installed.\nUse `pip install wheel`.\nExiting.")
-        sys.exit()
-    if os.system("pip list | grep twine"):
-        print("twine not installed.\nUse `pip install twine`.\nExiting.")
-        sys.exit()
-    os.system("python setup.py sdist bdist_wheel")
-    if sys.argv[-1] == 'publishtest':
-        os.system("twine upload -r test dist/*")
-    else:
-        os.system("twine upload dist/*")
-        print("You probably want to also tag the version now:")
-        print("  git tag -a {0} -m 'version {0}'".format(VERSION))
-        print("  git push --tags")
-    sys.exit()
-
-setup(
-    name = NAME,
-    version = VERSION,
-    description = DESC,
-    long_description = README,
-    author = AUTHOR,
-    author_email = AUTHOR_EMAIL,
-    maintainer = AUTHOR,
-    maintainer_email = AUTHOR_EMAIL,
-    license = LICENSE,
-    url = URL,
-    download_url = DOWNLOAD_URL,
-    packages = ["tx_nmodl"],
-    include_package_data=True,
-    install_requires = ["textX"],
-    keywords = "tools language DSL",
-    classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Intended Audience :: Developers',
-        'Intended Audience :: Information Technology',
-        'Intended Audience :: Science/Research',
-        'Topic :: Software Development :: Libraries :: Python Modules',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        ]
-
-)
+setup(name='tx_nmodl',
+      version='0.1',
+      description='NMODL parsing and compiling tools',
+      url='http://github.com/borismarin/nmodl-lems',
+      author='Boris Marin',
+      author_email='borimsarin@gmail.com',
+      license='GPL3',
+      packages=['tx_nmodl'],
+      package_data={'tx_nmodl': ['tests/sample_mods/*.mod']},
+      zip_safe=False,
+      setup_requires=['pytest-runner'],
+      install_requires=['textX'],
+      tests_require=['pytest', 'textX', 'xmltodict'],
+      )
--- tx_nmodl/tests/compiling/test_lemsgen.py
+++ tx_nmodl/tests/compiling/test_lemsgen.py
@@ -1,10 +1,11 @@
 import os
 from tx_nmodl.lems import mod2lems
 
-mod = os.path.join(os.path.dirname(__file__), 'kd.mod')
-with open(mod) as f:
+def get_sample(fname):
+    return os.path.join(os.path.dirname(__file__), 'sample_mods', fname)
+
+with open(get_sample('kd.mod')) as f:
     from xml.dom import minidom
-    print()
     print(minidom.parseString(mod2lems(f.read())).toprettyxml(indent="   "))
 
     # http://python-xmlunittest.readthedocs.io/en/latest/xmlunittest.html#xml-documents-comparison-assertion

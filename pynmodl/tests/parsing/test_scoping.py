4e8e22055e7a8c7e13ead6989b223a6de5c2dd33
borismarin <borismarin@gmail.com>
2018-01-18 19:47:43 -0200

    better scope testing bis

--- pynmodl/tests/parsing/test_scoping.py
+++ pynmodl/tests/parsing/test_scoping.py
@@ -38,12 +38,23 @@ def test_scoping():
         }
 
         DERIVATIVE dx {
+            LOCAL a
+            a = 1
             x' = f(x) + v : v is par
         }
 
-        """
+        FUNCTION alpha(x)(/ms){
+            LOCAL a
+            a = 0.1
+            if(fabs(x) > a){
+                   alpha=a*x/(1-exp(-x))
+            }else{
+                   alpha=a/(1-0.5*x)
+            }
+         }
+    """
     blocks = mm.model_from_str(p).blocks
-    (parameter, state, initial, function_f, derivative) = blocks
+    (parameter, state, initial, function_f, derivative, alpha) = blocks
 
     locals_in_init = children_of_type('Local', initial)
     assert refs_in(initial)[0].var == locals_in_init[0]
@@ -53,5 +64,14 @@ def test_scoping():
     assert refs_in(function_f)[2].var == locals_in_function_f[0]
     assert type(refs_in(function_f)[-1].var).__name__ == 'FuncPar'
 
-
     assert refs_in(derivative)[-1].var == parameter.parameters[0]
+
+    locals_in_alpha = children_of_type('Local', alpha)
+    alpha_a = locals_in_alpha[0]
+    alpha_x = alpha.pars[0]
+    assert refs_in(alpha)[0].var == alpha_a  # _a_ = 0.1
+    assert refs_in(alpha)[1].var == alpha_x  # fabs(_x_) > a
+    assert refs_in(alpha)[2].var == alpha_a  # fabs(x) > _a_
+    assert refs_in(alpha)[3].var == alpha  # _alpha_=a*x/(1-exp(-x))
+    assert refs_in(alpha)[4].var == alpha_a  # alpha=_a_*x/(1-exp(-x))
+    assert refs_in(alpha)[5].var == alpha_x  # alpha=a*_x_/(1-exp(-x))
